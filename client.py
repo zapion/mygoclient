6d553d835b9f0e450950e21078749c1819cff249
Paul Zapion Yang <zapionator@gmail.com>
2016-03-21 11:06:26 +0800

    wip

--- client.py
+++ client.py
@@ -1,69 +1,157 @@
 #!/usr/bin/python
+
+import sys
+import threading
+import asyncore
+import json
 from go_socket import GoSocket
-from commands.authenticate import Authenticate
-from commands.listgames import ListGames
-from commands.requestmatch import RequestMatch
-from commands.acceptmatchrequest import AcceptMatchRequest
-from commands.listplayers import ListPlayers
-from commands.play import Play
-from listeners.matchrequest import MatchRequest
-from listeners.boardupdate import BoardUpdate
 
 
-command_list = {
-    'authenticate': Authenticate,
-    'listgames': ListGames,
-    'requestmatch': RequestMatch,
-    'acceptmatchrequest': AcceptMatchRequest,
-    'listplayers': ListPlayers,
-    'play': Play,
-}
-
-listener_list = {
-    'matchrequest': MatchRequest,
-    'boardupdate': BoardUpdate,
-}
+def empty(*args):
+    sys.exit(0)
 
 
-def empty():
-    pass
+class Command():
+    @staticmethod
+    def authenticate(sock, context):
+        user = context.get('user')
+        password = context.get('password')
+        if user:
+            sock.buffer = user
+            sock.handle_write()
+        else:
+            raise AttributeError('user not specified')
+        # read here for test
+        sock.handle_read()
+        if password:
+            sock.buffer = password
+            sock.handle_write()
+        # read here for test
+        sock.handle_read()
+
+    @staticmethod
+    def list_games(sock, context):
+        # send list games command
+        command = ''
+        if sock.writable():
+            sock.buffer = command
 
 
 class Client():
-    def __init__(self, options):
-        self.add_all_commands()
+    def __init__(self, options=None):
+        self.user = 'zapbot'
+        self.password = '12345'
         if options:
             self.context = options
         else:
-            self.context = {}
+            self.context = {
+                'success': empty,
+            }
+        self.context['game'] = Game(self.user)
         # TODO: add an option here to enable debug mode
-        self.sock = GoSocket()
+        self.sock = GoSocket(self.context)
 
-    def connect(self, options):
-        self.authenticate(options)
+    def connect(self, options=None):
+        if not options:
+            options = {}
+            options['success'] = empty
         kwargs = {'host': '210.155.158.200',
                   'port': 6969,
-                  'fallback': self.listen}
+                  # 'fallback': self.listen
+                  'callback': Command.authenticate,
+                  'options': {'user': self.user,
+                              'password': self.password,
+                              },
+                  }
         self.sock.connect(**kwargs)
+        # self.input_thread.start()
+        asyncore.loop()
 
     def disconnect(self):
         self.sock.disconnect()
 
-    def listen(self, data):
-        for lstn_name, lstn_type in listener_list.items():
-            listener = lstn_type(self.sock, self.context)
-            if listener.parse(data):
-                listener.run()
-                break
 
-    def add_command(self, command):
-        name = command[0]
-        instance = command[1](self.sock, self.context)
-        setattr(self, name, instance)
-        callback = instance.run(self.context)
-        if not callback:
-            callback = self.listen
+class TestInput(threading.Thread):
+    def __init__(self, go_socket):
+        self.go_socket = go_socket
+        threading.Thread.__init__(self)
 
-    def add_all_commands(self):
-        for item in command_list.items():
-            self.add_command(item)
+    def run(self):
+        pass
+
+
+class RawInput(threading.Thread):
+    def __init__(self, go_socket):
+        self.go_socket = go_socket
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            send_data = raw_input()
+            if send_data:
+                self.go_socket.buffer = send_data
+
+
+class RuleHandler(object):
+    @staticmethod
+    def parse(line):
+        raise NotImplemented
+
+
+class Game(object):
+    '''
+    storing data
+    '''
+    def __init__(self, username):
+        self.username = username
+        self.inplay = False
+        self.board = []
+        self.player_list = []
+        self.game_list = []
+
+
+class BoardUpdate(RuleHandler):
+    @staticmethod
+    def parse(line):
+        pass
+
+
+class signum():
+    '''
+    This class is mostly a memo for strange numbers
+    '''
+    info = 1  # '1 1' is prompt login, '1 5' connect info
+    invalid_password = 5
+    game_info = 7
+    game_end = 9
+    game_stat = 15
+    stored_game = 18
+    game_result = 20
+    connect = 21
+    add_score = 22
+    opponent = 24
+    player_info = 27
+    undid = 28
+    match_request = 36
+    entry = 39
+    adjourn = 48
+    game_update = 49
+
+
+class DataParser(object):
+    rule_handlers = []
+
+    def __init__(self, options):
+        self.context = options
+
+    def parse(self, data):
+        lines = data.splitlines()
+        for line in lines:
+            for rule in self.rule_handlers:
+                rule.parse(line)
+
+
+if __name__ == '__main__':
+    config = json.load(open("config.json"))
+    cc = Client(config)
+    cc.connect()
--- go_socket.py
+++ go_socket.py
@@ -2,29 +2,99 @@
 
 import asyncore
 import socket
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s |%(name)s| %(message)s',
+    filename='/tmp/mygoclient.log',
+    filemode='a',
+)
+logger = logging.getLogger(__name__)
+MAX_RECV = 8192
 
 
-def empty():
+class signum():
+    '''
+    This class is mostly a memo for strange numbers
+    '''
+
+    info = 1  # '1 1' is prompt login, '1 5' connect info
+    invalid_password = 5
+    game_info = 7
+    game_end = 9
+    game_stat = 15
+    stored_game = 18
+    game_result = 20
+    connect = 21
+    add_score = 22
+    opponent = 24
+    player_info = 27
+    undid = 28
+    match_request = 36
+    entry = 39
+    adjourn = 48
+    game_update = 49
+
+
+def empty(*args):
     pass
 
 
 class GoSocket(asyncore.dispatcher):
     def __init__(self, context):
+        asyncore.dispatcher.__init__(self)
         self.context = context
-        self.callback = empty
+        if context.get('callback'):
+            self.callback = context.get('callback')
+        self.buffer = ""
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self, kwargs):
-        if kwargs['fallback']:
+    def connect(self, **kwargs):
+        if kwargs.get('fallback'):
             self.fallback = kwargs['fallback']
         else:
             self.fallback = empty
-        asyncore.dispatcher.connect(self, kwargs['host'], kwargs['port'])
+        host = kwargs['host']
+        port = kwargs['port']
+        logger.info("connect to [{0}:{1}]".format(host, port))
+        asyncore.dispatcher.connect(self, (host, port))
+        if kwargs.get('options'):
+            options = kwargs.get('options')
+        else:
+            options = {}
+        self.callback(self, options)
+
+    def disconnect(self):
+        self.send('quit\n')
 
     def run(self, options):
         if options.fallback:
             self.fallback = options.fallback
         self.fallback = empty
-        if options.success:
-            self.handle_connect = options.success
-        self.connect
+        if options.get('success'):
+            self.handle_connect = options.get('success')
+
+    def handle_connect(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+
+    def writable(self):
+        return (len(self.buffer) > 0)
+
+    def handle_write(self):
+        if self.buffer:
+            if not self.buffer.endswith("\n"):
+                self.buffer += "\n"
+            sent = self.send(self.buffer)
+            self.buffer = self.buffer[sent:]
+
+    def handle_read(self):
+        data = self.recv(MAX_RECV)
+        logger.debug(data)
+        if self.callback:
+            self.callback(data)
+        else:
+            self.fallback()
